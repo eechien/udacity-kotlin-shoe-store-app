21916caedfc26a7227fded210f4697d1cc9c4833
Emily Chien <echien3@gmail.com>
2020-12-20 17:31:21 -0700

    feat: Login only if we dont have user
    Set WelcomeFragment to startDestination
    Adds UserViewModel to keep track of logging in state

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -4,29 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
+
+// TODO Not show back button when on this view
 class LoginFragment : Fragment() {
+
+    companion object {
+        const val LOGIN_SUCCESSFUL: String = "LOGIN_SUCCESSFUL"
+    }
+
+    private val userViewModel: UserViewModel by activityViewModels()
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var savedStateHandle: SavedStateHandle
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false
         )
 
+        savedStateHandle = findNavController().previousBackStackEntry!!.savedStateHandle
+        savedStateHandle.set(LOGIN_SUCCESSFUL, false)
+
         binding.loginButton.setOnClickListener{ view: View ->
-            view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            login()
         }
         binding.createAccountButton.setOnClickListener{ view: View ->
-            view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            login()
         }
-
+        userViewModel.signInComplete.observe(viewLifecycleOwner, Observer { complete ->
+            if (complete == null) {
+              // do nothing
+            } else if (complete) {
+                savedStateHandle.set(LOGIN_SUCCESSFUL, true)
+                findNavController().popBackStack()
+                userViewModel.completeSignIn()
+            } else {
+                showErrorMessage()
+            }
+        })
         return binding.root
     }
-    // TODO, actually do something with the email+password
+
+    private fun login() {
+        val email = binding.emailInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        userViewModel.signIn(email, password)
+    }
+
+    private fun showErrorMessage() {
+        Toast.makeText(this.context, "Must submit an email and password!", Toast.LENGTH_SHORT)
+    }
 }
--- app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,19 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
 
 class WelcomeFragment : Fragment() {
 
     private lateinit var binding: FragmentWelcomeBinding
 
+    private val userViewModel: UserViewModel by activityViewModels()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome, container, false
         )
+        val navController = findNavController()
+
+        if (userViewModel.user.value == null) {
+            navController.navigate(R.id.loginFragment)
+        }
+
         return binding.root
     }
 }
